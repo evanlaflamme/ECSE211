e256fcf73c171430da5dbe4a7c72f165d1e8aed1
evanlaflamme <evanlaflamme@hotmail.com>
2017-09-11 20:03:44 -0400

    Work on bang-bang controller

--- Lab 1/src/ca/mcgill/ecse211/wallfollowing/UltrasonicController.java
+++ Lab 1/src/ca/mcgill/ecse211/wallfollowing/UltrasonicController.java
@@ -1,9 +1,5 @@
 package ca.mcgill.ecse211.wallfollowing;
 
-import lejos.hardware.port.;
-import lejos.hardware.sensor.;
-import lejos.remote.ev3.;
-
 public interface UltrasonicController {
 
   public void processUSData(int distance);
--- Lab 1/src/ca/mcgill/ecse211/wallfollowing/WallFollowingLab.java
+++ Lab 1/src/ca/mcgill/ecse211/wallfollowing/WallFollowingLab.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse211.wallfollowing; //Test
+package ca.mcgill.ecse211.wallfollowing;
 
 import lejos.hardware.sensor.*;
 import lejos.hardware.ev3.LocalEV3;
@@ -11,13 +11,12 @@ public class WallFollowingLab {
 
   // Parameters: adjust these for desired performance
 
-  private static final int bandCenter = 20; // Offset from the wall (cm)
-  private static final int bandWidth = 3; // Width of dead band (cm)
-  private static final int motorLow = 100; // Speed of slower rotating wheel (deg/sec)
+  private static final int bandCenter = 40; // Offset from the wall (cm)
+  private static final int bandWidth = 5; // Width of dead band (cm)
+  private static final int motorLow = 50; // Speed of slower rotating wheel (deg/sec)
   private static final int motorHigh = 200; // Speed of the faster rotating wheel (deg/seec)
 
-
-  private static final Port usPort = LocalEV3.get().getPort("S1");
+  private static final Port usPort = LocalEV3.get().getPort("S1");  
   public static final EV3LargeRegulatedMotor leftMotor =
       new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
   public static final EV3LargeRegulatedMotor rightMotor =
