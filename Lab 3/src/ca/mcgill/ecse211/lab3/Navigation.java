56f9e9b4f3e6e88ea1943d5d2d3cce8bba4e5fa1
evanlaflamme <evanlaflamme@hotmail.com>
2017-09-30 15:17:07 -0400

    Comment code and format properly

--- Lab 3/src/ca/mcgill/ecse211/lab3/Navigation.java
+++ Lab 3/src/ca/mcgill/ecse211/lab3/Navigation.java
@@ -5,106 +5,101 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 public class Navigation {
   private static final int FORWARD_SPEED = 190;
   private static final int ROTATE_SPEED = 150;
-  
-  //Bounds of the board
-  private static final double MAX_X = 3 * NavigationLab.SQUARE_LENGTH;
-  private static final double MIN_X = -1 * NavigationLab.SQUARE_LENGTH;
-  
-  private static final double MAX_Y = 3 * NavigationLab.SQUARE_LENGTH;
-  private static final double MIN_Y = -1 * NavigationLab.SQUARE_LENGTH;
-  
+
   private Odometer odometer;
-  
+
   private EV3LargeRegulatedMotor leftMotor;
   private EV3LargeRegulatedMotor rightMotor;
-  
-  public Navigation(Odometer odometer, EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor) {
+
+  public Navigation(Odometer odometer, EV3LargeRegulatedMotor leftMotor,
+      EV3LargeRegulatedMotor rightMotor) {
     this.odometer = odometer;
     this.leftMotor = leftMotor;
     this.rightMotor = rightMotor;
   }
-  
-  void travelTo(double x, double y) {    
-    x = x * NavigationLab.SQUARE_LENGTH; //Convert to odometer coordinates
+
+  // Travels to specified x and y from current position
+  void travelTo(double x, double y) {
+    x = x * NavigationLab.SQUARE_LENGTH; // Convert to odometer coordinates
     y = y * NavigationLab.SQUARE_LENGTH;
-    
-    double currentX = odometer.getX();
+
+    double currentX = odometer.getX(); // Get current odometer values
     double currentY = odometer.getY();
     double currentTheta = odometer.getTheta();
-    
-    double angle = Math.atan2(x - currentX, y - currentY); //Get angle between points
-    angle = angle * 180 / Math.PI; //Convert to degree
-    angle = positiveModulus(angle, 360); //Must be within 0 and 360
-    
-    if (currentTheta != angle) { //Ensure that the angle is the minimum
+
+    double angle = Math.atan2(x - currentX, y - currentY); // Get angle between points
+    angle = angle * 180 / Math.PI; // Convert to degree
+
+    if (currentTheta != angle) {
       double turnTheta = angle - currentTheta;
-      
-      if (turnTheta <= -180 && turnTheta >= -359) {
-        turnTheta += 360;
-      } else if (turnTheta >= 180 && turnTheta <= 359) {
-        turnTheta -= 360;
-      }
-      
-      turnTo(turnTheta);
+
+      turnTo(turnTheta); // Turn to new angle
     }
-    
-    double distance = Math.sqrt(Math.pow(y - currentY, 2) + Math.pow(x - currentX, 2));
-    
+
+    double distance = Math.sqrt(Math.pow(y - currentY, 2) + Math.pow(x - currentX, 2)); // Distance
+                                                                                        // to travel
+
     leftMotor.setSpeed(FORWARD_SPEED);
     rightMotor.setSpeed(FORWARD_SPEED);
-    
+
+    // Travel to point
     leftMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance), true);
     rightMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance), true);
   }
-  
-  void turnTo(double theta) {    
+
+  // Turns to theta relative to current angle
+  void turnTo(double theta) {
+    if (theta <= -180 && theta >= -359) { // Ensures that robot takes minimum angle
+      theta += 360;
+    } else if (theta >= 180 && theta <= 359) {
+      theta -= 360;
+    }
+
     leftMotor.setSpeed(ROTATE_SPEED);
     rightMotor.setSpeed(ROTATE_SPEED);
-    
+
+    // Rotate to new angle
     leftMotor.rotate(convertAngle(NavigationLab.WHEEL_RADIUS, NavigationLab.TRACK, theta), true);
     rightMotor.rotate(-convertAngle(NavigationLab.WHEEL_RADIUS, NavigationLab.TRACK, theta), false);
   }
-  
-  void avoidObject() {
-    double distance = (NavigationLab.SQUARE_LENGTH * 1.5);
-    
-    double currentX = odometer.getX();
-    double currentY = odometer.getY();
-    double currentTheta = odometer.getTheta();
-    
-    double rightTheta = 90 - currentTheta;
-    double rightXComponent = Math.sin(rightTheta) * distance;
-    double rightYComponent = Math.cos(rightTheta) * distance;
-    
-    double leftTheta = 90 + currentTheta;
-    double leftXComponent = Math.cos(leftTheta) * distance;
-    double leftYComponent = Math.sin(leftTheta) * distance;
-    
-    if (currentX + rightXComponent < MAX_X && currentX + rightXComponent > MIN_X &&
-        currentY + rightYComponent < MAX_Y && currentY + rightYComponent > MIN_Y) {
-      turnTo(positiveModulus(currentTheta + 90, 360));
-    } else if (currentX + leftXComponent < MAX_X && currentX + leftXComponent > MIN_X &&
-        currentY + leftYComponent < MAX_Y && currentY + leftYComponent > MIN_Y) {
-      turnTo(positiveModulus(currentTheta - 90, 360));
-    }
-    
+
+  // First step of avoidance
+  // Travel distance 90 degrees from current position to avoid object
+  void avoidObject_1(int direction, double distance) {
+    turnTo(90 * direction);
+
     leftMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance), true);
-    rightMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance), false);
+    rightMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance), true);
   }
-  
+
+  // Second step of avoidance
+  // Travel back in direction of point relative to original position of robot
+  void avoidObject_2(int direction, double distance) {
+    turnTo(-90 * direction);
+
+    leftMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance * 1.5), true);
+    rightMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance * 1.5), true);
+  }
+
+  // Third and last step of avoidance
+  // Return to exact opposite side of the obstacle relative to original position of robot
+  void avoidObject_3(int direction, double distance) {
+    turnTo(-90 * direction);
+
+    leftMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance), true);
+    rightMotor.rotate(convertDistance(NavigationLab.WHEEL_RADIUS, distance), true);
+  }
+
+  // Returns true if the robot is moving
   boolean isNavigating() {
     return leftMotor.isMoving() && rightMotor.isMoving();
   }
-  
+
   private static int convertDistance(double radius, double distance) {
     return (int) ((180.0 * distance) / (Math.PI * radius));
   }
 
   private static int convertAngle(double radius, double width, double angle) {
     return convertDistance(radius, Math.PI * width * angle / 360.0);
-  }
-  
-  private double positiveModulus(double num, int val) {
-    return ((num) % val + val) % val;
   }
 }
--- Lab 3/src/ca/mcgill/ecse211/lab3/NavigationLab.java
+++ Lab 3/src/ca/mcgill/ecse211/lab3/NavigationLab.java
@@ -14,84 +14,97 @@ import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MedianFilter;
 
 public class NavigationLab {
-  
+
   private static boolean objectDetected = false;
 
   private static final EV3LargeRegulatedMotor leftMotor =
       new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
-  
+
   private static final EV3LargeRegulatedMotor rightMotor =
       new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
-  
+
   private static final Port usPort = LocalEV3.get().getPort("S4");
-  
+
   private static Odometer odometer = null;
-  
-  private static ArrayList<int[]> points = new ArrayList<int[]>();
-  private static int pointsIndex = 0;
+
+  private static ArrayList<int[]> points = new ArrayList<int[]>(); // Array to hold the points
+  private static int pointsIndex = 0; // Holds the current index in the array
 
   public static final double WHEEL_RADIUS = 2.2;
   public static final double TRACK = 15.9;
-  public static final double SQUARE_LENGTH = 30.7; 
-  
-  private static final int DISTANCE_THRESHOLD = 15;
-  
+  public static final double SQUARE_LENGTH = 30.7;
+
+  private static final int DISTANCE_THRESHOLD = 10;
+
+  // Bounds of the board
+  private static final double MAX_X = 3 * NavigationLab.SQUARE_LENGTH;
+  private static final double MIN_X = -1 * NavigationLab.SQUARE_LENGTH;
+  private static final double MAX_Y = 3 * NavigationLab.SQUARE_LENGTH;
+  private static final double MIN_Y = -1 * NavigationLab.SQUARE_LENGTH;
+
+  // Sets the object detected boolean safely (for multiple threads to access)
   private synchronized static void setObjectDetection(boolean b) {
     objectDetected = b;
   }
-  
+
+  // Gets the object detected boolean safely
   private synchronized static boolean getObjectDetection() {
     return objectDetected;
   }
 
-  public static void main(String[] args) {    
-    //Map 1
-    /*points.add(new int[] {0, 2}); (-7, -2.4); (4.4, -2.1)(2.3, 0.1)
+  public static void main(String[] args) {
+    // Map 1
+    /*
+     * points.add(new int[] {0, 2}); points.add(new int[] {1, 1}); points.add(new int[] {2, 2});
+     * points.add(new int[] {2, 1}); points.add(new int[] {1, 0});
+     */
+
+    // Map 2
+    /*
+     * points.add(new int[] {1, 1}); points.add(new int[] {0, 2}); points.add(new int[] {2, 2});
+     * points.add(new int[] {2, 1}); points.add(new int[] {1, 0});
+     */
+
+    // Map 3
+    /*
+     * points.add(new int[] {1, 0}); points.add(new int[] {2, 1}); points.add(new int[] {2, 2});
+     * points.add(new int[] {0, 2}); points.add(new int[] {1, 1});
+     */
+
+    // Map 4
+    /*
+     * points.add(new int[] {0, 1}); points.add(new int[] {1, 2}); points.add(new int[] {1, 0});
+     * points.add(new int[] {2, 1}); points.add(new int[] {2, 2});
+     */
+
+    // Test Map
+    /*
+     * points.add(new int[] {2, 1}); points.add(new int[] {1, 1}); points.add(new int[] {1, 2});
+     * points.add(new int[] {2, 0});
+     */
+
+    // Test Map 2
+    points.add(new int[] {0, 2});
     points.add(new int[] {1, 1});
     points.add(new int[] {2, 2});
-    points.add(new int[] {2, 1});
-    points.add(new int[] {1, 0});*/
-    
-    //Map 2
-    /*points.add(new int[] {1, 1});
-    points.add(new int[] {0, 2});
-    points.add(new int[] {2, 2});
-    points.add(new int[] {2, 1});
-    points.add(new int[] {1, 0});*/
-    
-    //Map 3
-    /*points.add(new int[] {1, 0});
-    points.add(new int[] {2, 1});
-    points.add(new int[] {2, 2});
-    points.add(new int[] {0, 2});
-    points.add(new int[] {1, 1});*/
-    
-    //Map 4
-    /*points.add(new int[] {0, 1});
-    points.add(new int[] {1, 2});
-    points.add(new int[] {1, 0});
-    points.add(new int[] {2, 1});
-    points.add(new int[] {2, 2});*/
-    
-    //Test Map
-    points.add(new int[] {2, 1});
-    points.add(new int[] {1, 1});
-    points.add(new int[] {1, 2});
     points.add(new int[] {2, 0});
+    points.add(new int[] {1, 1});
 
     final TextLCD t = LocalEV3.get().getTextLCD();
     odometer = new Odometer(leftMotor, rightMotor);
     OdometryDisplay odometryDisplay = new OdometryDisplay(odometer, t);
-    
+
     // clear the display
     t.clear();
-    
+
     t.drawString("Press any button.", 0, 0);
     Button.waitForAnyPress();
-    
+
     odometer.start();
     odometryDisplay.start();
-    
+
+    // Thread for polling
+    // Started in main thread so that it has easy access to the object detected global variable
     Thread pollingThread = (new Thread() {
       public void run() {
         @SuppressWarnings("resource")
@@ -99,19 +112,20 @@ public class NavigationLab {
         SampleProvider usDistance = usSensor.getMode("Distance");
         SampleProvider usFiltered = new MedianFilter(usDistance, 5);
         float[] usData = new float[usFiltered.sampleSize()];
-        
+
         int distance;
-        
+
         while (true) {
           usFiltered.fetchSample(usData, 0); // acquire data
           distance = (int) (usData[0] * 100.0); // extract from buffer, cast to int
-          
-          t.drawString("Distance: " + distance, 0, 3);
-          
-          if (distance < DISTANCE_THRESHOLD && distance != 0) { //Ignore zero readings
-            setObjectDetection(true);
+
+          t.drawString("Distance: " + distance, 0, 3); // Show distane on the screen
+
+          if (distance < DISTANCE_THRESHOLD && distance != 0) { // If object detected ignoring zero
+                                                                // readings
+            setObjectDetection(true); // Set object detected to true
           }
-          
+
           try {
             Thread.sleep(50);
           } catch (Exception e) {
@@ -120,38 +134,108 @@ public class NavigationLab {
       }
     });
     pollingThread.start();
-    
-    try { //Wait 5 seconds
+
+    try { // Wait 5 seconds for ultrasonic sensor to activate
       Thread.sleep(5000);
     } catch (Exception e) {
     }
-  
-    while (pointsIndex < points.size()) {
+
+    while (pointsIndex < points.size()) { // While there are still points to visit
+      // Start navigation thread
       Thread navigationThread = (new Thread() {
         public void run() {
           Navigation n = new Navigation(odometer, leftMotor, rightMotor);
-          n.travelTo(points.get(pointsIndex)[0], points.get(pointsIndex)[1]);
-          
-          while (n.isNavigating()) { //While robot is moving
-            if (getObjectDetection()) { //If detect obstacle
-              leftMotor.stop(); //Stop motors
-              rightMotor.stop();
-              
-              n.avoidObject(); //Do avoidance
-              setObjectDetection(false);
-              return; //Leave thread
+          n.travelTo(points.get(pointsIndex)[0], points.get(pointsIndex)[1]); // Navigate to current
+                                                                              // point
+
+          if (detectObject(n)) { // Check for object detection
+            avoidObject(n); // Avoid object is detected
+            return;
+          }
+
+          pointsIndex++; // Only increment points index if object was not detected
+        }
+
+        // Returns true if object is detected during navigation
+        private boolean detectObject(Navigation n) {
+          while (n.isNavigating()) { // While robot is moving
+            if (getObjectDetection()) { // If detect obstacle
+              leftMotor.setSpeed(0); // Stop motors
+              rightMotor.setSpeed(0);
+
+              return true;
             }
           }
-          
-          pointsIndex++;
+
+          return false;
+        }
+
+        // Performs avoidance
+        private void avoidObject(Navigation n) {
+          double distance = NavigationLab.SQUARE_LENGTH; // The distance to travel
+          double avoidanceOffset = (NavigationLab.TRACK / 2) * 1.2; // Distance between center of
+                                                                    // rotation and end of robot
+
+          double currentX = odometer.getX(); // Get current odometer values
+          double currentY = odometer.getY();
+          double currentTheta = odometer.getTheta();
+
+          // Components if robot is to turn right
+          double rightTheta = 90 - currentTheta;
+          double rightXComponent = Math.sin(rightTheta) * distance;
+          double rightYComponent = Math.cos(rightTheta) * distance;
+
+          // Components if robot is to turn left
+          double leftTheta = 90 + currentTheta;
+          double leftXComponent = Math.cos(leftTheta) * distance;
+          double leftYComponent = Math.sin(leftTheta) * distance;
+
+          int direction = 0; // 1 signifies turning right; -1 signifies turning left
+
+          if (currentX + rightXComponent + avoidanceOffset < MAX_X
+              && currentX + rightXComponent - avoidanceOffset > MIN_X
+              && currentY + rightYComponent + avoidanceOffset < MAX_Y
+              && currentY + rightYComponent - avoidanceOffset > MIN_Y) { // If possible to turn
+                                                                         // right
+            direction = 1;
+          } else if (currentX + leftXComponent + avoidanceOffset < MAX_X
+              && currentX + leftXComponent - avoidanceOffset > MIN_X
+              && currentY + leftYComponent + avoidanceOffset < MAX_Y
+              && currentY + leftYComponent - avoidanceOffset > MIN_Y) { // If possible to turn left
+            direction = -1;
+          }
+
+          n.avoidObject_1(direction, distance); // Perform first step of avoidance
+
+          setObjectDetection(false); // Only start detection once first turn complete (don't want to
+                                     // detect same object twice)
+
+          if (detectObject(n)) { // Check for object during first step
+            avoidObject(n); // Avoid object and stop current avoidance
+            return;
+          }
+
+          n.avoidObject_2(direction, distance); // Perform second step of avoidance
+
+          if (detectObject(n)) { // Check for object during second step
+            avoidObject(n); // Avoid object and stop current avoidance
+            return;
+          }
+
+          n.avoidObject_3(direction, distance); // Perform third and last step of avoidance
+
+          if (detectObject(n)) { // Check for object during third step
+            avoidObject(n); // Avoid object and stop current avoidance
+            return;
+          }
         }
       });
       navigationThread.start();
-      
+
       try {
-        navigationThread.join(); //Wait for thread to finish
+        navigationThread.join(); // Wait for thread to finish before starting next navigation thread
       } catch (InterruptedException e) {
-        //Should never be interrupted
+        // Should never be interrupted
       }
     }
     System.exit(0);
